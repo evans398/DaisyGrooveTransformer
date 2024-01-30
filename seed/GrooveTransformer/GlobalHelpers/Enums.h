85ec46b75bca7e86e95851fd5169b205ca874e30
Nick Evans <nickevans@poblenou-129-158.eduroamcp.upf.edu>
2024-01-30 18:21:16 +0100

    Added Potentiometer struct and converted voice pots and groove pots

--- seed/GrooveTransformer/GlobalHelpers/Enums.h
+++ seed/GrooveTransformer/GlobalHelpers/Enums.h
@@ -102,3 +102,52 @@ enum class InterpolationButtonName {
     BUTTON_B
 };
 
+
+//******************************************************************************************/
+//** MIDI CC */
+//******************************************************************************************/
+enum MIDI_CC {
+    MIDI_CC_CHANNEL = 3,
+    A_BUTTON_SAVE = 4,
+    B_BUTTON_SAVE = 5,
+    A_BUTTON_RANDOMIZE = 6,
+    B_BUTTON_RANDOMIZE = 7,
+    CROSSFADER_POS = 8,
+    CLEAR = 9,
+    PLAY = 10, 
+    STOP = 11,
+    RECORD = 12,
+    RECORD_OFF = 13,
+    OVERDUB = 14,
+    TEMPO = 15,
+    UNCERTAINTY = 16,
+    GENERAL_PURPOSE_1 = 17,
+    GENERAL_PURPOSE_2 = 18,
+    GENERAL_PURPOSE_3 = 19,
+    GROOVE_VELOCITY = 20,
+    GROOVE_OFFSET = 21,
+    VOICE_VELOCITY_SCALE_1 = 22,
+    VOICE_VELOCITY_SCALE_2 = 23,
+    VOICE_VELOCITY_SCALE_3 = 24,
+    VOICE_VELOCITY_SCALE_4 = 25,
+    VOICE_VELOCITY_SCALE_5 = 26,
+    VOICE_VELOCITY_SCALE_6 = 27,
+    VOICE_DENSITY_SCALE_1 = 28,
+    VOICE_DENSITY_SCALE_2 = 29,
+    VOICE_DENSITY_SCALE_3 = 30,
+    VOICE_DENSITY_SCALE_4 = 31,
+    VOICE_DENSITY_SCALE_5 = 32,
+    VOICE_DENSITY_SCALE_6 = 33,
+    SAVE_1 = 34,
+    SAVE_2 = 35,
+    SAVE_3 = 36,
+    SAVE_4 = 37,
+    SAVE_5 = 38,
+    SAVE_6 = 39,
+    LOAD_1 = 40,
+    LOAD_2 = 41,
+    LOAD_3 = 42,
+    LOAD_4 = 43,
+    LOAD_5 = 44,
+    LOAD_6 = 45,
+};
--- seed/GrooveTransformer/UIComponents/Potentiometer.h
+++ seed/GrooveTransformer/UIComponents/Potentiometer.h
@@ -1,25 +1,28 @@
 #pragma once
-#include "../Managers/HardwareManager.h"
-#include "../Managers/UartLibreManager.h"
 #include "UIComponentHelper.h"
+#include "../Managers/HardwareManager.h"
+#include "../Managers/MidiManager.h"
 
-struct VoiceDensityPot {
-    int voice_idx;
+struct Potentiometer {
+    int midi_cc;
     int mux_channel;
     int mux_idx;
     HardwareManager* hardware_manager;
+    MidiManager* midi_manager;
     float prev_scaled_value;
 
-    VoiceDensityPot(
-        int voice_idx, 
+    Potentiometer(
+        int midi_cc, 
         int mux_channel, 
         int mux_idx, 
-        HardwareManager* hardware_manager
+        HardwareManager* hardware_manager,
+        MidiManager* midi_manager
     ) {
-        this->voice_idx = voice_idx;
+        this->midi_cc = midi_cc;
         this->mux_channel = mux_channel;
         this->mux_idx = mux_idx;
         this->hardware_manager = hardware_manager;
+        this->midi_manager = midi_manager;
         prev_scaled_value = GetScaledValue();
         TransmitNewValue(true);
     }
@@ -35,7 +38,7 @@ struct VoiceDensityPot {
     void TransmitNewValue(bool force_transmit){
         int scaled_value = GetScaledValue();
         if (scaled_value != prev_scaled_value || force_transmit) {
-            int voice_number = voice_idx + 1;
+            midi_manager->SendMidiCC(10, midi_cc, scaled_value);
         }
         this->prev_scaled_value = scaled_value;
     }
